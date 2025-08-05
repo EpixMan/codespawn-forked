7a103dd7cdfa637b52501734ac4168942e68e2f8
EpixMan <abdullahgtgt@gmail.com>
2025-08-05 19:34:59 +0000

    updated dependancies

---
+++ target/debug/deps/codespawn-36257323142ed6e2.d
@@ -0,0 +1,18 @@
+/home/first/git/epix/codespawn-forked/target/debug/deps/codespawn-36257323142ed6e2.d: src/lib.rs src/error.rs src/raw_code.rs src/fmt_code.rs src/xml_parser.rs src/json_parser.rs src/string_gen/mod.rs src/string_gen/keywords.rs src/string_gen/cpp.rs src/string_gen/rust.rs
+
+/home/first/git/epix/codespawn-forked/target/debug/deps/libcodespawn-36257323142ed6e2.rlib: src/lib.rs src/error.rs src/raw_code.rs src/fmt_code.rs src/xml_parser.rs src/json_parser.rs src/string_gen/mod.rs src/string_gen/keywords.rs src/string_gen/cpp.rs src/string_gen/rust.rs
+
+/home/first/git/epix/codespawn-forked/target/debug/deps/libcodespawn-36257323142ed6e2.rmeta: src/lib.rs src/error.rs src/raw_code.rs src/fmt_code.rs src/xml_parser.rs src/json_parser.rs src/string_gen/mod.rs src/string_gen/keywords.rs src/string_gen/cpp.rs src/string_gen/rust.rs
+
+src/lib.rs:
+src/error.rs:
+src/raw_code.rs:
+src/fmt_code.rs:
+src/xml_parser.rs:
+src/json_parser.rs:
+src/string_gen/mod.rs:
+src/string_gen/keywords.rs:
+src/string_gen/cpp.rs:
+src/string_gen/rust.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.3.3
